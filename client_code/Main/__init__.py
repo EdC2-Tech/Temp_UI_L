bea27a7784ac2f6a7692c0901904c61c883fbd72
c_edward@live.ca <c_edward@live.ca>
2025-04-03 19:33:16 +0000

    Edited Main; Deleted Group_T.ItemTemplate1, Group_T.ItemTemplate2, Group_T.ItemTemplate3, Group_T.ItemTemplate4 and 4 more

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -24,7 +24,7 @@ class Main(MainTemplate):
 
     # Initiate default homepage panel 
     self.content_panel.clear()
-    self.content_panel.add_component(Group_T(), full_width_row=True)
+    self.content_panel.add_component(Resource_T(), full_width_row=True)
 
     # Pre-load existing data tables
     #self.update_tables()
